23b3095c3bf86d55b443ef6b409498ac972c19c5
Kirsty Leishman <leishman.kirsty@gmail.com>
2022-12-02 19:36:02 +0000

    ex005 1sand0s

--- challenges/exercise005.js
+++ challenges/exercise005.js
@@ -2,11 +2,22 @@ export const findNextNumber = (nums, n) => {
   if (nums === undefined) throw new Error("nums is required");
   if (n === undefined) throw new Error("n is required");
   // Your code here!
+  const nIndex = nums.indexOf(n);
+  return nIndex > -1 && nIndex < (nums.length - 1) ? nums[nIndex + 1] : null;
 };
 
 export const count1sand0s = (str) => {
   if (str === undefined) throw new Error("str is required");
   // Your code here!
+  const digits = str.split("");
+  const count = digits.reduce((acc, curr) => {
+    const currCount = acc[curr];
+    return {
+      ...acc,
+      [curr]: currCount + 1
+    }
+  }, {0: 0, 1: 0});
+  return count;
 };
 
 export const reverseNumber = (n) => {
